42cb04ed1e8e0a393128fe338ec7c54a4419435a
Hashim Baig <hashim.baig17@gmail.com>
2025-08-06 20:41:51 +0530

    Feat Hero Text, searchbar icon

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,23 @@ export default function Home() {
 
   return (
       <>
-          <SearchBar/>
+          <section className="flex justify-center w-full my-10">
+              <section className="flex flex-col items-center gap-7.5 w-[745px] px-12">
+
+                  <h1 className="font-dm-sans font-bold text-[56px] leading-[60px] tracking-[-0.01em] text-center ">
+                      Find{" "}
+                      <span className="bg-gradient-to-r from-[#D6C7FF] to-[#AB8BFF] bg-clip-text text-transparent">
+                    Movies
+                  </span>{" "}
+                      You’ll Love Without the Hassle
+                  </h1>
+
+                  <SearchBar/>
+
+              </section>
+          </section>
+
+
           <AllMoviesGrid  />
       </>
   );
--- components/molecules/SearchBar/SearchBarTemplate.tsx
+++ components/molecules/SearchBar/SearchBarTemplate.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import {Input} from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
 
 
 const SearchBarTemplate: React.FC = ({}) => {
   return (
-    <div>
-      <Input
-        placeholder={"Search thousands of movies..."}
-      />
-    </div>
+      <div className="relative w-full max-w-md">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-placeholderBlue" />
+          </div>
+
+          <Input
+              placeholder="Search thousands of movies..."
+              className="pl-10 placeholder:text-placeholderBlue placeholder:italic text-placeholderBlue focus:outline-none"
+          />
+      </div>
   );
 };
 
