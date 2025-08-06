66d2ff58682fa2d68b2092cc3c2bb10254d66ee1
Hashim Baig <hashim.baig17@gmail.com>
2025-08-07 01:23:03 +0530

    Feat Pagination fetching

---
+++ components/molecules/Pagination/Pagination.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PaginationTemplate from './PaginationTemplate';
+import {PaginationProps} from "@/types";
+import {getPageItems} from "@/lib/utils";
+
+const Pagination: React.FC<PaginationProps> = ({totalPageCount, currentPage, setCurrentPage}) => {
+
+  const handleNextPageChange = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    if (currentPage < totalPageCount) setCurrentPage(currentPage + 1);
+  }
+
+  const handlePrevPageChange = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    if (currentPage > 1) setCurrentPage(currentPage - 1);
+  }
+
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  }
+
+  const isPrevDisabled = currentPage === 1;
+  const isNextDisabled = currentPage === totalPageCount;
+
+  const pageItems = getPageItems(currentPage, totalPageCount);
+
+  return <PaginationTemplate
+      totalPageCount={totalPageCount}
+      handleNextPageChange={handleNextPageChange}
+      handlePreviousPageChange={handlePrevPageChange}
+      handlePageChange={handlePageChange}
+      currentPage={currentPage}
+      isPrevDisabled={isPrevDisabled}
+      isNextDisabled={isNextDisabled}
+      pageItems={pageItems}
+  />;
+};
+
+export default Pagination;
---
+++ components/molecules/Pagination/PaginationTemplate.tsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+import {Button} from "@/components/ui/button";
+import {PaginationTemplateProps} from "@/types";
+
+const PaginationTemplate: React.FC<PaginationTemplateProps> = ({
+                                                                 totalPageCount,
+                                                                 handleNextPageChange,
+                                                                 handlePreviousPageChange,
+                                                                   handlePageChange,
+                                                                 currentPage,
+                                                                  isPrevDisabled,
+                                                                  isNextDisabled,
+                                                                    pageItems,
+}) => {
+  return (
+      <section className="pb-10">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <Button
+                  variant="ghost"
+                  asChild
+                  aria-disabled={isPrevDisabled}
+                  tabIndex={isPrevDisabled ? -1 : undefined}
+                  className={isPrevDisabled ? "pointer-events-none opacity-50" : ""}
+                  onClick={e => {
+                    if (isPrevDisabled) {
+                      e.preventDefault();
+                      return;
+                    }
+                    handlePreviousPageChange(e);
+                  }}
+              >
+                <PaginationPrevious href="#" />
+              </Button>
+            </PaginationItem>
+
+              {pageItems.map((item, idx) => {
+                  if (item === "ellipsis") {
+                      return (
+                          <PaginationItem key={"ellipsis-" + idx}>
+                              <PaginationEllipsis />
+                          </PaginationItem>
+                      );
+                  }
+
+                  return (
+                      <PaginationItem key={item}>
+                          <Button
+                              variant={item === currentPage ? "outline" : "ghost"}
+                              asChild
+                              className={item > 10000 ? "w-12 text-center truncate" : ""}
+                          >
+                              <PaginationLink
+                                  href="#"
+                                  isActive={item === currentPage}
+                                  onClick={(e) => {
+                                      e.preventDefault();
+                                      if (item !== currentPage) handlePageChange(item);
+                                  }}
+                              >
+                                  {item}
+                              </PaginationLink>
+                          </Button>
+                      </PaginationItem>
+                  );
+              })}
+
+              <PaginationItem>
+                  <Button
+                      variant="ghost"
+                      asChild
+                      aria-disabled={isNextDisabled}
+                      tabIndex={isNextDisabled ? -1 : undefined}
+                      className={isNextDisabled ? "pointer-events-none opacity-50" : ""}
+                      onClick={e => {
+                          if (isNextDisabled) {
+                              e.preventDefault();
+                              return;
+                          }
+                          handleNextPageChange(e);
+                      }}
+                  >
+                      <PaginationNext href="#" />
+                  </Button>
+              </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+        <p className="text-center py-2">Total Pages: {totalPageCount}</p>
+        <p className="text-center py-2">Current Page: {currentPage}</p>
+      </section>
+  );
+};
+
+export default PaginationTemplate;
